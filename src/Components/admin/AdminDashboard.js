32b91cb2215fa9ef63509f2b49a725ab3bf3b729
imnulhaqueruman <imnulhaquerumantalukder1999@gmail.com>
2021-07-26 12:03:37 +0600

    fetching all products in admin dashboard

--- src/Components/admin/AdminDashboard.js
+++ src/Components/admin/AdminDashboard.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import AdminNav from '../Nav/AdminNav';
+import{getProductsByCount} from '../../functions/Product';
+
+
 const AdminDashboard = () => {
+    const[products,setProducts] = useState([])
+    const[loading,setLoading]  = useState(false)
+
+    useEffect(() =>{
+         loadAllProducts()
+    },[])
+
+    const loadAllProducts = () =>{
+        setLoading(true)
+        getProductsByCount(100)
+        .then((res) =>{
+            setProducts(res.data)
+            setLoading(false)
+        })
+        .catch((err) =>{
+            console.log(err)
+            setLoading(false)
+        } );
+    }
     return (
         <div className="container-fluid">
             <div className="row">
                 <div className="col-md-2">
                     <AdminNav></AdminNav>
                 </div>
-
+                {loading ? (<h4 className="text-danger">Loading..</h4>) : (<h4>All Products...</h4>)}
                  <div className="col">
-                   Admin Dashboard 
+                   {JSON.stringify(products)} 
                  </div>
             </div>
             
