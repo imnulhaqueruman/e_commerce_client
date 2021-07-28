40024888f0ded80f34599afdefb22f3e4bb24497
imnulhaqueruman <imnulhaquerumantalukder1999@gmail.com>
2021-07-29 00:18:05 +0600

    fetch all product in product component

---
+++ src/Components/Cards/ProductCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductCard = ({product}) => {
+    return (
+        <div>
+            {product.title}
+        </div>
+    );
+};
+
+export default ProductCard;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,40 @@
-import React from 'react';
-
+import React,{useState,useEffect} from 'react';
+import{getProductsByCount} from '../../functions/Product';
+import ProductCard from '../Cards/ProductCard';
 const Home = () => {
+
+    const[products,setProducts] = useState([])
+    const[loading,setLoading] = useState(false)
+    
+    useEffect(() =>{
+      loadAllProducts()
+    },[])
+    
+    const loadAllProducts = () =>{
+        setLoading(true)
+        getProductsByCount(3)
+        .then(res =>{
+            setProducts(res.data);
+            setLoading(false)
+        })
+    }
+
     return (
-        <div>
-            
+       <>
+        <div className="jumbotron">
+          {loading ? (<h4>Loading....</h4>) : (<h4>All Product</h4>)}
+          
         </div>
+        <div className="container">
+           <div className="row">
+              {products.map((product) =>
+                   <div key={product._id} className="col-md-4">
+                        <ProductCard product={product} />
+                   </div>
+              )}
+           </div>
+        </div>
+       </>
     );
 };
 
