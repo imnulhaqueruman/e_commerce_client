878c8b95b98451dd6fe31ddecca09dc405ca0bfb
imnulhaqueruman <imnulhaquerumantalukder1999@gmail.com>
2021-07-30 14:50:33 +0600

    image carousel create

--- src/Components/Cards/SingleProduct.js
+++ src/Components/Cards/SingleProduct.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {Card} from 'antd';
 import{HeartOutlined,ShoppingCartOutlined} from '@ant-design/icons';
 import { Link } from 'react-router-dom';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 const {Meta} = Card
 const SingleProduct = ({product}) => {
@@ -9,7 +11,13 @@ const SingleProduct = ({product}) => {
     return (
         <>
             <div className="col-md-7">
-                  Image Carousel
+                  <Carousel showArrows={true}
+                   autoPlay 
+                   infiniteLoop
+                   >
+                      {images && images.map(i =>
+                         <img src={i.url} key={i.public_id} alt=""/>)}
+                  </Carousel>
             </div>
             <div className="col-md-5">
                
