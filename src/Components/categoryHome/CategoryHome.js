df315450d868eb85c0d6b1ac8323d84391570b79
imnulhaqueruman <imnulhaquerumantalukder1999@gmail.com>
2021-09-09 01:34:32 +0600

    category page with products

--- src/Components/categoryHome/CategoryHome.js
+++ src/Components/categoryHome/CategoryHome.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import{getCategory} from '../../functions/Category';
+import ProductCard from './../Cards/ProductCard';
 
 const CategoryHome = () => {
     const{slug} = useParams()
@@ -13,13 +14,35 @@ const CategoryHome = () => {
         setLoading(true)
         getCategory(slug).then(c =>{
             console.log(JSON.stringify(c.data,null,4))
-            setCategory(c.data)
-            
+            setCategory(c.data.category)
+            setProducts(c.data.products);
+            setLoading(false);
+
         })
     },[])
     return (
-        <div>
-            {slug}
+        <div className='container-fluid'>
+            <div className="row">
+                <div className="col">
+                  {loading ? (
+                        <h4 className="text-center p-3 mt-5 mb-5 display-3 bg-secondary">Loading...</h4>
+                  ) :(
+                <h4 className="text-center p-3 mt-5 mb-5 display-3 bg-secondary">
+                  {products.length} Products in {category.name} category
+                </h4>
+                  )
+                }
+                </div>
+            </div>
+            <div className="row">
+                {products.map((p) =>
+                <div className="col-md-4" key={p._id}>
+                        <ProductCard product={p}/>
+                </div>
+                )
+                }
+            </div>
+            
         </div>
     );
 };
