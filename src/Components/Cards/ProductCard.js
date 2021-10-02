8bd8d5002a2b045e064188bf81f974cc687db267
imnulhaqueruman <imnulhaquerumantalukder1999@gmail.com>
2021-10-02 12:29:52 +0600

    add to cart item saved local storage

--- src/Components/Cards/ProductCard.js
+++ src/Components/Cards/ProductCard.js
@@ -4,9 +4,33 @@ import laptop from '../../images/laptop.png';
 import { EyeOutlined,ShoppingCartOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 import { showAverage } from '../../functions/rating';
+
+import _ from "lodash";
 const{Meta} = Card;
 const ProductCard = ({product}) => {
     const{images,title,description,slug,price} = product
+    const handleAddToCart = () =>{
+        // create cart array 
+        let cart = []
+        if(typeof window !== 'undefined'){
+            // if cart is in local storage get it
+            if(localStorage.getItem('cart')){
+                cart = JSON.parse(localStorage.getItem('cart'));
+            }
+            // push new products to cart
+            cart.push({
+                ...product,
+                count: 1,
+            });
+            // remove duplicates
+            let unique = _.uniqWith(cart, _.isEqual)
+            // save to local storage 
+            console.log('unique', unique)
+            localStorage.setItem('cart', JSON.stringify(unique))
+
+
+        }
+    }
     return (
       <>
          {product && product.ratings && product.ratings.length > 0 ?
@@ -31,7 +55,7 @@ const ProductCard = ({product}) => {
                 <Link to={`/products-update/${slug}`}>
                         <EyeOutlined className="text-warning"/> <br/> View Product
                 </Link>,
-                    <><ShoppingCartOutlined  className="text-danger"/> <br/> Add to Cart</>
+                    <a onClick={handleAddToCart}><ShoppingCartOutlined  className="text-danger"/> <br/> Add to Cart</a>
                 ]
             }
         >
