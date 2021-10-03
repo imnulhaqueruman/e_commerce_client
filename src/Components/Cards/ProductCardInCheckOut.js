f6cf83cd47805d3eeb0ceaa5047612fe016bb357
imnulhaqueruman <imnulhaquerumantalukder1999@gmail.com>
2021-10-03 21:43:56 +0600

    color pickin cart page

--- src/Components/Cards/ProductCardInCheckOut.js
+++ src/Components/Cards/ProductCardInCheckOut.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import { Image } from 'antd';
 import laptop from '../../images/laptop.png';
+import{useDispatch} from 'react-redux'
 
 const ProductCardInCheckOut = ({p}) =>{
-    
+    const colors = ["Black","Brown","Silver","White","Blue"]
+    let dispatch = useDispatch()
+    const handleColorChange = (e) =>{
+     console.log(e.target.value)
+     let cart = []
+     if(typeof window !== 'undefined'){
+         if(localStorage.getItem('cart')){
+             cart = JSON.parse(localStorage.getItem('cart'));
+         }
+         cart.map((product,i) =>{
+             if(product._id === p._id){
+                 cart[i].color = e.target.value;
+             }
+         });
+         console.log('update', cart)
+        localStorage.setItem('cart', JSON.stringify(cart));
+        dispatch({
+           type:'ADD_TO_CART',
+           payLoad:cart
+        })
+     }
+    }
     return(
         <tbody>
             <tr>
@@ -17,7 +39,22 @@ const ProductCardInCheckOut = ({p}) =>{
                 <td>{p.title}</td>
                 <td>{p.price}</td>
                 <td>{p.brand}</td>
-                <td>{p.color}</td>
+                <td>
+                    <select 
+                        onChange ={handleColorChange}
+                        className="form-control"
+                        name="color"
+                        id=""
+                    >
+                      { p.color ? <option value={p.color}>{p.color}</option>
+                       :<option>select</option>
+                      }
+                      {colors.filter((c) =>c !==p.color)
+                        .map((c) =>
+                         <option key={c}value={c}>{c}</option>)
+                      }
+                    </select>
+                </td>
                 <td>{p.count}</td>
                 <td>shipping</td>
                 <td>Remove</td>
