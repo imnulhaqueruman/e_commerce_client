2e224cb256291256276fd18ae26e743386279046
imnulhaqueruman <imnulhaquerumantalukder1999@gmail.com>
2022-01-23 00:44:12 +0600

    show out of stock

--- src/Components/Cards/ProductCard.js
+++ src/Components/Cards/ProductCard.js
@@ -73,9 +73,9 @@ const ProductCard = ({product}) => {
                         <EyeOutlined className="text-warning"/> <br/> View Product
                 </Link>,
                     <Tooltip title={toolTip}>
-                        <a onClick={handleAddToCart}>
+                        <a onClick={handleAddToCart} disabled={product.quantity <1}>
                             <ShoppingCartOutlined  className="text-danger"/> 
-                            <br/> Add to Cart</a>
+                            <br/>{product.quantity <1 ? 'Out of stock' :'Add to Cart'}</a>
                     </Tooltip>
                 ]
             }
