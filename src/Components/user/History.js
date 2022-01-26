39049672a9705aaec2b0879065bc6336a3a2a4df
imnulhaqueruman <imnulhaquerumantalukder1999@gmail.com>
2022-01-27 00:27:14 +0600

    show each order in table

--- src/Components/user/History.js
+++ src/Components/user/History.js
@@ -21,9 +21,32 @@ const History = () => {
         })
     const showOrderInTable = (order) =>(
         <>
-           <p>
-            each order and it's products
-          </p>
+           <table className="table table-bordered">
+               <thead className="thead-light">
+                 <tr>
+                     <th scope="col">Title</th>
+                     <th scope="col">Price</th>
+                     <th scope="col">Brand</th>
+                     <th scope="col">Color</th>
+                     <th scope="col">Count</th>
+                     <th scope="col">Shipping</th>
+                 </tr>
+               </thead>
+               <tbody>
+                   {order.products.map((p,i) =>(
+                       <tr key={i}>
+                           <td>
+                               <b>{p.product.title}</b>
+                           </td>
+                           <td>{p.product.price}</td>
+                           <td>{p.product.Brand}</td>
+                           <td>{p.color}</td>
+                           <td>{p.count}</td>
+                           <td>{p.product.shipping === "Yes" ? <CheckCircleOutlined style={{color:'green'}}/> : <CloseCircleOutlined style={{color:'red'}}/>}</td>
+                       </tr>
+                   ))}
+               </tbody>
+           </table>
         </>
         
     )
