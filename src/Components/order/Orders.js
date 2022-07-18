36bc55a1b037b0ad5deee34eda82be5c53618c64
imnulhaqueruman <imnulhaquerumantalukder1999@gmail.com>
2022-07-19 03:20:13 +0600

    oder table in dashboard

--- src/Components/order/Orders.js
+++ src/Components/order/Orders.js
@@ -3,6 +3,35 @@ import {CheckCircleOutlined,CloseCircleOutlined} from "@ant-design/icons"
 import ShowPaymentInfo from './../user/ShowPaymentInfo';
 
 const Orders = ({orders,handleStatusChange}) => {
+    const showOrderInTable = (order) =>(
+        <>
+           <table className="table table-bordered">
+               <thead className="thead-light">
+                 <tr>
+                     <th scope="col">Title</th>
+                     <th scope="col">Price</th>
+                     <th scope="col">Brand</th>
+                     <th scope="col">Color</th>
+                     <th scope="col">Count</th>
+                     <th scope="col">Shipping</th>
+                 </tr>
+               </thead>
+               <tbody>
+                   {order.products.map((p,i) =>(
+                       <tr key={i}>
+                           <td>
+                               <b>{p.product.title}</b>
+                           </td>
+                           <td>{p.product.price}</td>
+                           <td>{p.product.Brand}</td>
+                           <td>{p.color}</td>
+                           <td>{p.count}</td>
+                           <td>{p.product.shipping === "Yes" ? <CheckCircleOutlined style={{color:'green'}}/> : <CloseCircleOutlined style={{color:'red'}}/>}</td>
+                       </tr>
+                   ))}
+               </tbody>
+           </table>
+        </>)
     return (
         <div>
           {orders.map((order) =>(
@@ -25,11 +54,12 @@ const Orders = ({orders,handleStatusChange}) => {
                         </div>
                     </div>
                 </div>
-               
+             {showOrderInTable(order)}  
             </div>
           )
 
-          )}  
+          )}
+        
         </div>
     );
 };
